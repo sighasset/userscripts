1c3fd0c56fb517235721733db6e160a89be37f1c
sighasset <lewdjdoe@gmail.com>
2026-07-12 20:31:08 +0300

    fix(f95zone): make Page Navigation global

--- f95zone/page-nav/page-nav.meta.js
+++ f95zone/page-nav/page-nav.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         f95zone: Page Navigation
+// @name         Page Navigation
 // @namespace    https://github.com/sighasset
-// @version      0.1
-// @description  F1 - previous, F2 - next
+// @version      0.2
+// @description  Navigate pages with F1 (previous) and F2 (next)
 // @author       sighasset
-// @match        https://f95zone.to/sam/latest_alpha/*
+// @match        https://f95zone.to/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=f95zone.to
 // @updateURL    https://raw.githubusercontent.com/sighasset/userscripts/main/f95zone/page-nav/page-nav.meta.js
 // @downloadURL  https://raw.githubusercontent.com/sighasset/userscripts/main/f95zone/page-nav/page-nav.user.js
--- f95zone/page-nav/page-nav.user.js
+++ f95zone/page-nav/page-nav.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         f95zone: Page Navigation
+// @name         Page Navigation
 // @namespace    https://github.com/sighasset
-// @version      0.1
-// @description  F1 - previous, F2 - next
+// @version      0.2
+// @description  Navigate pages with F1 (previous) and F2 (next)
 // @author       sighasset
-// @match        https://f95zone.to/sam/latest_alpha/*
+// @match        https://f95zone.to/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=f95zone.to
 // @updateURL    https://raw.githubusercontent.com/sighasset/userscripts/main/f95zone/page-nav/page-nav.meta.js
 // @downloadURL  https://raw.githubusercontent.com/sighasset/userscripts/main/f95zone/page-nav/page-nav.user.js
@@ -16,16 +16,22 @@
 
   document.addEventListener('keyup', (e) => {
     if (e.code === 'F1') {
-      queryPrevBtn().click();
+      queryPrevBtn()?.click();
     } else if (e.code === 'F2') {
-      queryNextBtn().click();
+      queryNextBtn()?.click();
     }
   });
 
   function queryNextBtn() {
-    return document.querySelector('a.nav_next');
+    return (
+      document.querySelector('a.nav_next') ??
+      document.querySelector('a.pageNav-jump--next')
+    );
   }
   function queryPrevBtn() {
-    return document.querySelector('a.nav_prev');
+    return (
+      document.querySelector('a.nav_prev') ??
+      document.querySelector('a.pageNav-jump--prev')
+    );
   }
 })();
